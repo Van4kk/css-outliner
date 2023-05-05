7114b5e0aab0a074af03205be95d0bd8b436c31f
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-05-05 23:42:24 +0300

    getting some ideas from other extensions

---
+++ src/js/app/GetCss.js
@@ -0,0 +1,76 @@
+import {extractCSS, filterRules, getNumber, removeDuplicateCSS} from "../utils/helpers";
+
+class GetAppliedCSS {
+    constructor(el) {
+        // el.matches = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector || el.oMatchesSelector;
+        this.element = el;
+    }
+
+    get rules() {
+        const slice = Function.call.bind(Array.prototype.slice);
+        return Array.from(document.styleSheets).reduce((rulesArr, sheet) => {
+            try {
+                if (sheet?.cssRules) {
+                    return rulesArr.concat(slice(sheet.cssRules));
+                }
+            } catch {
+                return rulesArr;
+            }
+        }, []);
+    }
+
+    all() {
+        const { css: mediaCSS, hover: mediaHover } = this.mediaQueryCSS();
+        const { css: styleCSS, hover: styleHover } = this.css();
+        const inlineCSS = this.inlineCSS();
+
+        const css = removeDuplicateCSS(inlineCSS + mediaCSS + styleCSS);
+        const hover = removeDuplicateCSS(mediaHover + styleHover);
+
+        return { css, hover };
+    }
+
+    inlineCSS() {
+        const styleAttribute = this.element.getAttribute('style');
+
+        if (!styleAttribute) return '';
+
+        return styleAttribute.endsWith(';') ? styleAttribute : `${styleAttribute};`;
+    }
+
+    mediaQueryCSS() {
+        const mediaQueries = this.rules.filter(({ conditionText, media }) => media && window.matchMedia(conditionText).matches && getNumber(conditionText));
+        const mediaQueriesSorted = mediaQueries.sort((a, b) => {
+            const aMedia = getNumber(a.conditionText);
+            const bMedia = getNumber(b.conditionText);
+
+            if (aMedia > bMedia) return -1;
+            if (bMedia > aMedia) return 1;
+
+            return 0;
+        });
+
+        return mediaQueriesSorted.reduce(
+            (result, media) => {
+                if (media.cssRules) {
+                    const filteredRules = filterRules(this.element, Array.from(media.cssRules));
+                    const {css, hover} = extractCSS(filteredRules);
+
+                    result.css += css;
+                    result.hover += hover;
+
+                    return result;
+                }
+
+                return result;
+            },
+            {css: '', hover: ''},
+        );
+    }
+
+    css() {
+        const filteredRules = filterRules(this.element, this.rules);
+
+        return extractCSS(filteredRules);
+    }
+}
---
+++ src/js/app/HtmlElement.js
@@ -0,0 +1,44 @@
+import {filterClasses, generateBoxModels, removePx} from "../utils/helpers";
+
+export default class HtmlElement {
+    constructor(reference) {
+        this.reference = reference;
+        this.computedStyleKeys = [
+            ...generateBoxModels('margin'),
+            ...generateBoxModels('padding'),
+            'fontFamily',
+        ];
+    }
+
+    getSelector() {
+        return {
+            tag: this.reference.tagName.toLowerCase(),
+            id: this.reference.id ? `#${this.reference.id}` : '',
+            classes: filterClasses(Array.from(this.reference.classList)),
+        };
+    }
+
+    getSize() {
+        const { height, width } = this.reference.getBoundingClientRect();
+
+        return { height, width };
+    }
+
+    getComputedStyles() {
+        return this.computedStyleKeys.reduce((styles, key) => {
+            const value = getComputedStyle(this.reference)[key];
+
+            styles[key] = key === 'fontFamily' ? value.split(',')[0].replace(/"/g, '') : removePx(value);
+
+            return styles;
+        }, {});
+    }
+
+    getAll() {
+        return {
+            selector: this.getSelector(),
+            size: this.getSize(),
+            computedStyles: this.getComputedStyles(),
+        };
+    }
+}
