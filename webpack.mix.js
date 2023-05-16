461df87bc0e6601d0c13a36a230e8555263d627c
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-05-16 16:16:19 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,15 @@
 let mix = require('laravel-mix');
 
 mix.setPublicPath('./')
-    .sass('src/assets/styles/scss/main.scss', 'dist/css')
-    .js('src/assets/js/background.js', 'dist/js')
-    .js('src/assets/js/main.js', 'dist/js').vue()
-    .copy('src/assets/icons/', 'dist/assets/icons/')
-    .copy('src/assets/icons/logo/', 'dist/assets/logo/')
-    .copy('src/assets/fonts/', 'dist/assets/fonts/')
-    .copy('src/manifest.json', 'dist/manifest.json')
-    .copy('src/assets/_locales/', 'dist/_locales/')
-    .copy('src/assets/*.html', 'dist/')
+    .sass('src/assets/styles/scss/main.scss', 'chrome/css')
+    .js('src/assets/js/background.js', 'chrome/js')
+    .js('src/assets/js/main.js', 'chrome/js').vue()
+    .copy('src/assets/icons/', 'chrome/assets/icons/')
+    .copy('src/assets/icons/logo/', 'chrome/assets/logo/')
+    .copy('src/assets/fonts/', 'chrome/assets/fonts/')
+    .copy('src/manifest.json', 'chrome/manifest.json')
+    .copy('src/assets/_locales/', 'chrome/_locales/')
+    .copy('src/assets/*.html', 'chrome/')
     .options({
         processCssUrls: false
     });
