6d7ccb84e8b64d9f514594657fd9e4de3d946f82
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-05 23:33:57 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,19 +6,8 @@ mix.setPublicPath('./')
             extensions: [".*",".wasm",".mjs",".js",".jsx",".json",".vue",".*",".html",".css",".svg"],
         },
     })
-    //.sass('src/assets/styles/scss/main.scss', 'chrome/css')
-    .js('src/app/*.js', 'chrome/js/')
-    .js('src/*.js', 'chrome/')
-    .js('src/app/main.js', 'chrome/js').vue()
-    .copy('src/app/assets/icons/*', 'chrome/assets/icons/')
-    .copy('src/app/assets/icons/logo/*', 'chrome/assets/logo/')
-    .copy('src/app/assets/fonts/', 'chrome/assets/fonts/')
+    .copy('src/background.js', 'chrome/js/')
+    .js('src/app/css-outliner.js', 'chrome/js').vue()
+    .copy('src/app/assets/logo/*', 'chrome/assets/logo/')
     .copy('src/manifest.json', 'chrome/')
-    .copy('src/_locales/', 'chrome/_locales/')
-    .copy('src/app/assets/*.html', 'chrome/')
-    .postCss('src/app/assets/styles/css/maintailwind.css', 'chrome/css/main.css', [
-        require("tailwindcss")
-    ])
-    .options({
-        processCssUrls: false
-    });
+    .copy('src/*.html', 'chrome/')
