84a6e9608dacccbccbb5701a559d5e63cb14c9d9
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-09 11:16:57 +0300

    Create menus.js

---
+++ src/app/utils/menus.js
@@ -0,0 +1,32 @@
+export default {
+    navigation: {
+        id: {
+            name: '',
+            icon: '',
+        }
+    },
+    mainDropdown: {
+        id: {
+            name: '',
+            icon: '',
+        }
+    },
+    appActiveMenu: {
+        id: {
+            name: '',
+            icon: '',
+        }
+    },
+    debugDropdown: {
+        id: {
+            name: '',
+            icon: '',
+        }
+    },
+    ex: {
+        id: {
+            name: '',
+            icon: '',
+        }
+    }
+}
