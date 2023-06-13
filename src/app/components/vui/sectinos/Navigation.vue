5bbab49d586a50e479aee8c13f613ec32e834372
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-13 14:23:13 +0300

    Done navigation

--- src/app/components/vui/sectinos/Navigation.vue
+++ src/app/components/vui/sectinos/Navigation.vue
@@ -1,20 +1,42 @@
 <template>
-    <div id="css-outliner-navigation-menu" :class="className">
-        <button>Page 1</button>
-        <button>Page 2</button>
-        <button>Page 3</button>
-        <button>Page 4</button>
-        <button>Page 5</button>
+    <div id="css-outliner-navigation-menu">
+        <button v-for="nav in navigationData"
+                :key="nav.id"
+                :title="nav.name"
+                @click="switchActivePage(nav.id, nav.id)"
+                :class="{
+                    'bg-opacity-5 text-primary': nav.id === activePage,
+                    'hover:text-white': nav.id !== activePage
+                }"
+                class="transition-colors"
+        >
+            <Icon class="w-[31px] h-[31px]" :name="nav.icon" :active="nav.id === activePage" />
+        </button>
     </div>
 </template>
 <script>
+import { ref, shallowReactive } from "vue";
+import { navigation } from "../../../utils/menus";
+import Icon from "../Icon.vue";
+
 export default {
     name: 'Navigation',
-    props: {
-        className: {
-            type: String,
-            default: ''
-        },
-    }
+    components: { Icon },
+    emits: ['changeActiveMenu'],
+    setup(props, { emit }) {
+        const navigationData = shallowReactive(navigation)
+        const activePage = ref('');
+
+        const switchActivePage = (pageId, elementId) => {
+            activePage.value = pageId;
+            emit('changeActiveMenu', pageId, elementId);
+        }
+
+        return {
+            navigationData,
+            activePage,
+            switchActivePage,
+        }
+    },
 };
 </script>
--- src/app/utils/menus.js
+++ src/app/utils/menus.js
@@ -1,32 +1,56 @@
-export default {
-    navigation: {
-        id: {
-            name: '',
-            icon: '',
-        }
+
+export const navigation = [
+    {
+        id: 'element-properties',
+        name: 'Element Properties',
+        icon: 'cursor',
+        active: true
     },
-    mainDropdown: {
-        id: {
-            name: '',
-            icon: '',
-        }
+    {
+        id: 'page-assets',
+        name: 'Page Assets',
+        icon: 'document-search',
+        active: true
     },
-    appActiveMenu: {
-        id: {
-            name: '',
-            icon: '',
-        }
+    {
+        id: 'page-color-palette',
+        name: 'Page Color Palette',
+        icon: 'palette',
+        active: true
     },
-    debugDropdown: {
-        id: {
-            name: '',
-            icon: '',
-        }
+    {
+        id: 'tools',
+        name: 'Cool Tools',
+        icon: 'wrench',
+        active: true
     },
-    ex: {
-        id: {
-            name: '',
-            icon: '',
-        }
-    }
-}
+    {
+        id: 'settings',
+        name: 'App Settings',
+        icon: 'settings',
+        active: true
+    },
+];
+    //menu items inside components if it's the case
+export const appActiveMenu = [
+    {
+        id: '',
+        name: '',
+        icon: '',
+    },
+];
+export const debugDropdown = [
+    {
+        id: '',
+        name: '',
+        icon: '',
+    },
+];
+
+export const ex = [
+    {
+        id: '',
+        name: '',
+        icon: '',
+    },
+];
