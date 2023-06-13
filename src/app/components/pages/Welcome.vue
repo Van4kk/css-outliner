8837c8dc65707632bc10d9cafa3a86374a011493
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-13 19:40:28 +0300

    done with welcome

--- src/app/components/pages/Welcome.vue
+++ src/app/components/pages/Welcome.vue
@@ -1,11 +1,26 @@
-<script setup>
-
-</script>
-
 <template>
-
+    <div id="css-outliner-welcome-wrapper">
+        <div id="css-outliner-welcome-content-wrapper">
+            <span>
+                <Icon name="click" />
+            </span>
+            <h1>Click an element to see the properties</h1>
+        </div>
+    </div>
 </template>
+<script>
+import Icon from "../vui/Icon.vue";
 
-<style scoped>
+export default {
+    name: 'Welcome',
+    components: { Icon },
+    setup() {
+        const hey = 'Hello From welcome';
 
-</style>
+        return {
+            hey,
+        }
+    }
+}
+//and the list of shortcuts under that cursor icon
+</script>
