f4c6b5432b587b85171558067f3d8925bc70061b
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-29 20:45:44 +0300

    for later

---
+++ src/app/components/vui/InputText.vue
@@ -0,0 +1,45 @@
+<template>
+    <label class=""
+           :class="labelClasses"
+           :for="inputName"
+    >{{ labelText }}</label>
+    <input class=""
+           :class="inputClasses"
+           :id="inputId"
+           :name="inputName"
+           :type="inputType"
+    />
+</template>
+<script>
+
+export default {
+    name: 'InputText',
+    props: {
+        labelClasses: {
+            type: String,
+            default: ''
+        },
+        labelText: {
+            type: String,
+            required: true
+        },
+        inputClasses: {
+            type: String,
+            default: ''
+        },
+        inputId: {
+            type: String,
+            default: ''
+        },
+        inputName: {
+            type: String,
+            required: true
+        },
+        inputType: {
+            type: String,
+            default: 'text',
+            validator: (type) => ['text', 'email', 'hidden', 'number', 'password', 'tel', 'search'].includes(type)
+        },
+    }
+}
+</script>
