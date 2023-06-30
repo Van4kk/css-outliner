a00ee384f817a27722790a83c594528a9a6b3c52
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-06-30 14:42:04 +0300

    update `ElementSelector.vue` with a bit accessibility

--- src/app/components/vui/sectinos/ElementDetails/ElementSelector.vue
+++ src/app/components/vui/sectinos/ElementDetails/ElementSelector.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="css-outliner-element-selectors">
+    <div id="css-outliner-element-selectors" :title="[ selector.tag, selector.id,  selector.classes ]">
         <span>{{ selector.tag }}</span>
         <span>{{ selector.id }}</span>
         <span>{{ selector.classes }}</span>
