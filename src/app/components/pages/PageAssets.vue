7950887a323a04e33db86d47e8d3d14af1396824
Van4kk <65396868+Van4kk@users.noreply.github.com>
2023-07-04 14:02:14 +0300

    Update PageAssets.vue

--- src/app/components/pages/PageAssets.vue
+++ src/app/components/pages/PageAssets.vue
@@ -2,9 +2,22 @@
     <page-wrapper>
         <h1>{{hello}}</h1>
     </page-wrapper>
+<!--  TODO: dropdown with sort -->
+<!--  TODO: export all assets -->
+<!--  TODO: on hover appear the name with `..` and the ability to export only this image -->
+<!--  TODO: show the size of the image -->
 </template>
-<script setup>
+<script>
 import PageWrapper from "../vui/sectinos/PageWrapper.vue";
 
-const hello = 'Hello From Page Assets'
+export default {
+    components: {PageWrapper},
+    setup() {
+        const hello = 'Hello From Page Assets'
+
+        return {
+            hello
+        }
+    }
+}
 </script>
